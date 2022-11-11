79e518477c722bd73ebb8af9646a5c463cd602e4
Max Rios <max.rios@oliant.io>
2022-11-11 17:21:28 -0600

    Basic tests performance in .Net 6

---
+++ LinqPerformance/Benchmarks.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqPerformance
+{
+    [MemoryDiagnoser(false)]
+    public class Benchmarks
+    {
+        [Params(100)]
+        public int Size { get; set; }
+
+        private IEnumerable<int> _items;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _items = Enumerable.Range(1, Size).ToArray();
+        }
+
+        [Benchmark]
+        public void Min() => _items.Min();
+
+        [Benchmark]
+        public void Max() => _items.Max();
+
+        [Benchmark]
+        public int Sum() => _items.Sum();
+
+        [Benchmark]
+        public int Max_Own()
+        {
+            var biggest = int.MinValue;
+            foreach(var item in _items)
+            {
+                biggest = item;
+            }
+            return biggest;
+        }
+    }
+}
---
+++ LinqPerformance/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+using System.Linq;
+
+namespace LinqPerformance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchmarks>();
+        }
+    }
+}
